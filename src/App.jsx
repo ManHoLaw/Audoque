ed0165c45d80faf398a031657e0ec1e73b4944d2
ManHoLaw <homanlaw1@hotmail.com>
2025-02-02 22:57:43 +0000

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import Title from "./components/Title"
 import Footer from "./components/Footer"
 import Logo from "./components/Logo"
+import { useState } from "react"
 
 const App = () => {
   const [selection, setSelection] = useState(null);
