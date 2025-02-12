de222754e4e289ba9d59512426dd918a5715cfc4
ManHoLaw <homanlaw1@hotmail.com>
2025-02-12 15:55:48 +0000

    general change

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -14,7 +14,7 @@ const Highlights = ({ selection }) => {
 
   return (
     <section className="flex justify-center overflow-hidden h-full pt-5">
-      <div className="w-3/4 grid grid-cols-2 md:grid-cols-3 content-evenly gap-4 sm:py-10 pb-10">
+      <div className="w-5/6 grid grid-cols-2 md:grid-cols-3 content-evenly gap-4 sm:py-10 pb-10">
         {cakes
           .filter((cake) => {
             if (isChefSelection) return cake.chefSelection;
@@ -41,7 +41,7 @@ const Highlights = ({ selection }) => {
                   ref={(el) => (imgRefs.current[index] = el)} // 🔹 Store ref properly
                   src={cake.img}
                   alt={cake.title}
-                  className="max-sm:object-cover flex max-w-50 w-3/4 rounded-2xl cursor-pointer"
+                  className="max-sm:object-cover flex max-w-50 w-full rounded-2xl cursor-pointer"
                   onClick={() => setPopupCake(cake)}
                 />
 
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,22 +1,21 @@
-
-const Popup = ({ closePopup, cake }) => {
+import { memo } from "react";
+const Popup = memo(({ closePopup, cake }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center"
-    style={{ backgroundColor: 'rgba(51, 51, 51, 0.5)' }} >
-      <div className="bg-white p-5 rounded-lg shadow-lg">
+    <div className="fixed inset-0 flex items-center justify-center transition-none " onClick={closePopup}>
+      <div className="bg-white p-5 rounded-lg shadow-lg" onClick={(e)=>e.stopPropagation()}>
         <h2>
-          <img
-                  src={cake.img}
-                  alt={cake.title}
-                  class="h-auto max-sm:object-cover flex max-w-100 rounded-2xl cursor-pointer max-sm:max-w-75"
-                />
+          <img src={cake.img} alt={cake.title} className="h-auto max-sm:object-cover flex max-w-100 rounded-2xl cursor-pointer max-sm:max-w-75" />
         </h2>
+        <div class='flex justify-center text-xl'>
+          {cake.title}
+        </div>
         <button onClick={closePopup} className="mt-2 px-4 py-2 bg-red-500 text-white rounded">
           Close
         </button>
       </div>
     </div>
-);
-}
+  );
+});
+
 
 export default Popup
