67bf46312111a3b4bd0315081fee2b4d321484f1
ManHoLaw <homanlaw1@hotmail.com>
2025-02-19 19:30:10 +0000

    added form to detail page

--- src/components/CakeDetail.jsx
+++ src/components/CakeDetail.jsx
@@ -1,10 +1,10 @@
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { cakes, Prices } from '../constants';
-import Popup from './Popup';
 
 const CakeDetail = () => {
   const { title } = useParams(); // Get cake ID from URL
   const cake = cakes.find((c) => c.title.toLowerCase().replace(/\s+/g, "-") === title);
+  const navigate = useNavigate();
   const priceObj = Prices.find(
     (p) =>
       p.type === cake.type &&
@@ -14,14 +14,27 @@ const CakeDetail = () => {
   if (!cake) return <h2 className='font-bold text-2xl'>Cake Not Found</h2>;
 
   return (
-    <section className="px-6 flex items-center flex-col z-0">
-        <div className='flex flex-1 pb-5'>
+    <section className="px-6 flex flex-col z-0">
+        <div className='flex flex-1 justify-center pb-5'>
             <h1 className="text-xl font-bold">{cake.title}</h1>
         </div>
 
 
         <div className='flex-1'>
             <img src={cake.img} alt={cake.title} className="max-w-100 max-sm:max-w-75 object-cover my-4 rounded-xl" />
+        </div>
+        <div class='flex justify-end'>
+          <Link
+              to={'/order-form'}
+              className='flex cursor-pointer p-2 rounded-md bg-white'
+              onClick={(e) => {
+                  setTimeout(() => {
+                      navigate('/order-form');
+                  }, 300);
+              }}
+          >
+            Order Form
+          </Link>
         </div>
         <div className='flex-col gap-3 flex px-6 max-sm:w-full max-sm:max-w-screen w-[calc(100vw*2/5)] my-3 overflow-hidden'>
             <p className='break-words whitespace-normal'>
@@ -32,8 +45,7 @@ const CakeDetail = () => {
             </div>
         </div>
 
-
-        <div class='flex justify-center'>
+        <div class='flex justify-center p-5'>
           {priceObj?.price?.map((price, index) => (
             <button
               key={index}
