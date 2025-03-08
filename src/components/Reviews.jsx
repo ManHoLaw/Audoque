d3d53cb04a1246b986e85a54040ed19c43df19f5
ManHoLaw <homanlaw1@hotmail.com>
2025-03-08 17:30:50 +0000

    test

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -15,7 +15,7 @@ const Reviews = () => {
         };
 
         try {
-            const response = await fetch("/api/addReview", {
+            const response = await fetch("api/addReview", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(newReview),
